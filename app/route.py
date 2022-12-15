8bec2dbac98df2bed160aa9da42c6f816355fbe9
J-BT <jeanbaptiste.theroulde@gmail.com>
2022-12-15 15:10:47 +0000

    ajout request.method == POST vide OK

--- app/route.py
+++ app/route.py
@@ -1,38 +1,48 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 from .db import get_db_config, db_connect
 import mysql.connector
+import requests # <- ne pas installer 
 
-
-@app.route('/')   # == @app.route('/index')
+@app.route('/', methods=["GET", "POST"])   # == @app.route('/index')
 def index():
 
-    # Afficher les valeurs utilisateurs dans le tableau puis db
-    # Mettre le cemin absolu de votre config.json
-    path = "/ISEN_Projects/Flask_WebScrapping/appli_ensemble/config.json"
-    config = get_db_config(path)
-    
-    try:
-        myDB = db_connect(config)
-        cursor = myDB.cursor()
-        dbOK = myDB.is_connected()
+    if request.method == "GET":
+
+        # Afficher les valeurs utilisateurs dans le tableau puis db
+        # Mettre le cemin absolu de votre config.json
+        path = "/ISEN_Projects/Flask_WebScrapping/appli_ensemble/config.json"
+        config = get_db_config(path)
         
-        query="""
-            SELECT * FROM `Utilisateur`;
-        """
-        cursor.execute(query)
-        result_select = cursor.fetchall()
+        try:
+            myDB = db_connect(config)
+            cursor = myDB.cursor()
+            dbOK = myDB.is_connected()
+            
+            query="""
+                SELECT * FROM `Utilisateur`;
+            """
+            cursor.execute(query)
+            result_select = cursor.fetchall()
 
-        return render_template('index.html',
-        configHTML=config, dbOK__=dbOK, HTML_Result=result_select)
-    
+            return render_template('index.html',
+            configHTML=config, dbOK__=dbOK, HTML_Result=result_select)
+        
 
-    except mysql.connector.Error as e:
-        return render_template('index.html', configHTML=config, error=e)
+        except mysql.connector.Error as e:
+            return render_template('index.html', configHTML=config, error=e)
 
-    # inserer des valeurs utilisateurs dans le tableau puis db
+        # inserer des valeurs utilisateurs dans le tableau puis db
 
-    
+    if request.method == "POST":
+
+        prenom = request.form["prenom"]
+        nom = request.form["nom"]
+        adresse_mail = request.form["adresse_mail"]
+
+        
+        return redirect(url_for("index"))
+
 
 """
 On veut une table : Utilisateur
